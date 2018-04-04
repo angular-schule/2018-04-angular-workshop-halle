91e59a66d73c8ace32ea334adee62a9c5b97a1a5
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-04-04 15:46:09 +0200

    Spread Operator

--- typescript-playground/app.ts
+++ typescript-playground/app.ts
@@ -1,5 +1,24 @@
 import { Customer } from "./customer";
 
 const myCustomer = new Customer(3, 'name');
-console.log(myCustomer.id);
-myCustomer.fooBar('abc');
+// console.log(myCustomer.id);
+// myCustomer.fooBar('abc');
+
+// ---------
+
+const myObject = {
+    name: 'Angular',
+    year: 2009
+}
+
+
+// const myObject2 = Object.assign({}, myObject, { year: 2016 });
+
+const clone = { ...myObject, year: 2016 };
+
+// myObject2.year = 2016;
+
+
+console.log(myObject);
+console.log(clone);
+console.log(myObject === clone);
