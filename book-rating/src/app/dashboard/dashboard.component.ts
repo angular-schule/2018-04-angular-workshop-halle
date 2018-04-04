c0891f705eea89b6d65e9b02db5a6bb6448c1ef4
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-04-04 14:06:58 +0200

    Property Bindings

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { Book } from '../shared/book';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  url = 'https://angular.schule';
 
   books: Book[];
 
