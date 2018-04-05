0f6eea108314249bf23fc3aecbcdbfd0608306c6
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-04-05 13:25:33 +0200

    TestBed: Rating anzeigen

--- book-rating/src/app/book/book.component.spec.ts
+++ book-rating/src/app/book/book.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 import { BookComponent } from './book.component';
 
@@ -10,16 +12,38 @@ describe('BookComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ BookComponent ]
     })
+    .overrideComponent(BookComponent, { // override nötig wegen OnPush
+      set: {
+        changeDetection: ChangeDetectionStrategy.Default
+      }
+    })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    component.book = {
+      isbn: '000',
+      title: '',
+      description: '',
+      rating: 3
+    };
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the correct rating', () => {
+    const ratingBox = fixture.debugElement.query(By.css('.testing-rating-box'));
+    expect(ratingBox.nativeElement.textContent).toBe('3');
+
+    component.book = { ...component.book, rating: 5 };
+    fixture.detectChanges();
+    expect(ratingBox.nativeElement.textContent).toBe('5');
   });
 });
